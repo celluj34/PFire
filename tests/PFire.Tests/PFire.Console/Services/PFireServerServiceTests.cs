d2d6f9ba06d5b1ea520885ce24b65109e6f80f63
Joe Cellucci <celluj34@gmail.com>
2020-12-14 02:01:33 -0600

    fix tests

--- tests/PFire.Tests/PFire.Console/Services/PFireServerServiceTests.cs
+++ tests/PFire.Tests/PFire.Console/Services/PFireServerServiceTests.cs
@@ -1,4 +1,5 @@
-﻿using System.Threading.Tasks;
+﻿using System.Threading;
+using System.Threading.Tasks;
 using Moq;
 using PFire.Console.Services;
 using PFire.Core;
@@ -12,32 +13,34 @@ namespace PFire.Tests.PFire.Console.Services
         public async Task StartAsync_Calls_Start()
         {
             //arrange
+            var cancellationToken = new CancellationToken();
             var pFireServerMock = _autoMoqer.GetMock<IPFireServer>();
 
             var service = _autoMoqer.CreateInstance<PFireServerService>();
 
             //act
-            await service.StartAsync(default);
+            await service.StartAsync(cancellationToken);
 
             //assert
-            pFireServerMock.Verify(x => x.Start(), Times.Once);
-            pFireServerMock.Verify(x => x.Stop(), Times.Never);
+            pFireServerMock.Verify(x => x.Start(cancellationToken), Times.Once);
+            pFireServerMock.Verify(x => x.Stop(cancellationToken), Times.Never);
         }
 
         [Fact]
         public async Task StopAsync_Calls_Stop()
         {
             //arrange
+            var cancellationToken = new CancellationToken();
             var pFireServerMock = _autoMoqer.GetMock<IPFireServer>();
 
             var service = _autoMoqer.CreateInstance<PFireServerService>();
 
             //act
-            await service.StopAsync(default);
+            await service.StopAsync(cancellationToken);
 
             //assert
-            pFireServerMock.Verify(x => x.Start(), Times.Never);
-            pFireServerMock.Verify(x => x.Stop(), Times.Once);
+            pFireServerMock.Verify(x => x.Start(cancellationToken), Times.Never);
+            pFireServerMock.Verify(x => x.Stop(cancellationToken), Times.Once);
         }
     }
 }
