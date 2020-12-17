24859a16a48f249dc525b47f821c98de7662c35d
Joe Cellucci <celluj34@gmail.com>
2020-12-16 22:37:26 -0600

    change factory into big switch/case

--- src/PFire.Core/Services/MessageTypeFactory.cs
+++ src/PFire.Core/Services/MessageTypeFactory.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
 using PFire.Core.Enums;
 using PFire.Core.Exceptions;
 using PFire.Core.Messages.Bidirectional;
@@ -15,86 +14,45 @@ namespace PFire.Core.Services
 
     internal class MessageTypeFactory : IMessageTypeFactory
     {
-        private static readonly Dictionary<XFireMessageType, Type> Messages = new Dictionary<XFireMessageType, Type>
-        {
-            {
-                XFireMessageType.ClientVersion, typeof(ClientVersion)
-            },
-            {
-                XFireMessageType.LoginRequest, typeof(LoginRequest)
-            },
-            {
-                XFireMessageType.LoginFailure, typeof(LoginFailure)
-            },
-            {
-                XFireMessageType.LoginSuccess, typeof(LoginSuccess)
-            },
-            {
-                XFireMessageType.ClientConfiguration, typeof(ClientConfiguration)
-            },
-            {
-                XFireMessageType.ConnectionInformation, typeof(ConnectionInformation)
-            },
-            {
-                XFireMessageType.Groups, typeof(Groups)
-            },
-            {
-                XFireMessageType.GroupsFriends, typeof(GroupsFriends)
-            },
-            {
-                XFireMessageType.ServerList, typeof(ServerList)
-            },
-            {
-                XFireMessageType.ChatRooms, typeof(ChatRooms)
-            },
-            {
-                XFireMessageType.GameInformation, typeof(GameInformation)
-            },
-            {
-                XFireMessageType.KeepAlive, typeof(KeepAlive)
-            },
-            {
-                XFireMessageType.Did, typeof(Did)
-            },
-            {
-                XFireMessageType.ServerChatMessage, typeof(ChatMessage)
-            },
-            {
-                XFireMessageType.UserLookup, typeof(UserLookup)
-            },
-            {
-                XFireMessageType.FriendRequest, typeof(FriendRequest)
-            },
-            {
-                XFireMessageType.FriendRequestAccept, typeof(FriendRequestAccept)
-            },
-            {
-                XFireMessageType.FriendRequestDecline, typeof(FriendRequestDecline)
-            },
-            {
-                XFireMessageType.NicknameChange, typeof(NicknameChange)
-            },
-            {
-                XFireMessageType.StatusChange, typeof(StatusChange)
-            }
-        };
-
         public Type GetMessageType(XFireMessageType messageType)
         {
-            // Hack: Client sends message type of 2 for chat messages but expects message type of 133 on receive...
-            // this is because the client to client message (type 2) is send via UDP to the clients directly,
-            // whereas 133 is a message routed via the server to the client
-            if (messageType == XFireMessageType.UDPChatMessage)
+            return messageType switch
             {
-                messageType = XFireMessageType.ServerChatMessage;
-            }
+                XFireMessageType.ChatContent => typeof(ChatContent),
+                XFireMessageType.LoginRequest => typeof(LoginRequest),
 
-            if (Messages.TryGetValue(messageType, out var message))
-            {
-                return message;
-            }
-
-            throw new UnknownMessageTypeException(messageType);
+                // Hack: Client sends message type of 2 for chat messages but expects message type of 133 on receive...
+                // This is because the client to client message (type 2) is send via UDP to the clients directly,
+                // whereas 133 is a message routed via the server to the client.
+                XFireMessageType.UDPChatMessage => typeof(ChatMessage),
+                XFireMessageType.ClientVersion => typeof(ClientVersion),
+                XFireMessageType.GameInformation => typeof(GameInformation),
+                XFireMessageType.FriendRequest => typeof(FriendRequest),
+                XFireMessageType.FriendRequestAccept => typeof(FriendRequestAccept),
+                XFireMessageType.FriendRequestDecline => typeof(FriendRequestDecline),
+                XFireMessageType.UserLookup => typeof(UserLookup),
+                XFireMessageType.KeepAlive => typeof(KeepAlive),
+                XFireMessageType.NicknameChange => typeof(NicknameChange),
+                XFireMessageType.ClientConfiguration => typeof(ClientConfiguration),
+                XFireMessageType.ConnectionInformation => typeof(ConnectionInformation),
+                XFireMessageType.StatusChange => typeof(StatusChange),
+                XFireMessageType.LoginChallenge => typeof(LoginChallenge),
+                XFireMessageType.LoginFailure => typeof(LoginFailure),
+                XFireMessageType.LoginSuccess => typeof(LoginSuccess),
+                XFireMessageType.FriendsList => typeof(FriendsList),
+                XFireMessageType.FriendsSessionAssign => typeof(FriendsSessionAssign),
+                XFireMessageType.ServerChatMessage => typeof(ChatMessage),
+                XFireMessageType.FriendInvite => typeof(FriendInvite),
+                XFireMessageType.ClientPreferences => typeof(ClientPreferences),
+                XFireMessageType.UserLookupResult => typeof(UserLookupResult),
+                XFireMessageType.ServerList => typeof(ServerList),
+                XFireMessageType.Groups => typeof(Groups),
+                XFireMessageType.GroupsFriends => typeof(GroupsFriends),
+                XFireMessageType.FriendStatusChange => typeof(FriendStatusChange),
+                XFireMessageType.ChatRooms => typeof(ChatRooms),
+                XFireMessageType.Did => typeof(Did),
+                _ => throw new UnknownMessageTypeException(messageType)
+            };
         }
     }
 }
